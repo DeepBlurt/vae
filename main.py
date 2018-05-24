7ab14b425fcfcadf707e29c3a23b6f52ed2c4f56
AnXiong <anxiong1994@163.com>
2018-05-24 21:54:24 +0800

    Nothing....

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 batch_size = 100
-epoch = 800
+epoch = 8000
 input_placeholder = tf.placeholder(tf.float32, [batch_size, 784])
 
 vae = Vae(input_placeholder, "./model/model.ckpt")
@@ -35,7 +35,7 @@ with tf.Session() as sess:
         err, _ = sess.run([cost, minimize], feed_dict={input_placeholder: batch})
         duration = time.time() - start
         print(duration, err)
-    # vae.save('./model/model.ckpt')
+    vae.save('./model/model.ckpt')
     batch, _ = mnist.train.next_batch(batch_size)
     recon = sess.run(reconstruct, feed_dict={input_placeholder: batch})
 
@@ -55,4 +55,3 @@ for i in range(n):
 plt.figure()
 plt.imshow(gallery, cmap="gray")
 plt.show()
-
